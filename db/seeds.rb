da8f5e9f21ed6bcbc74e02c9e5ccab5ec53d1351
Jayanth <jayanth.koralla@gmail.com>
2021-06-21 11:09:54 +0530

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "raghu", password: "password")
+User.create(username: "sarma", password: "password")
+User.create(username: "siri", password: "password")
+User.create(username: "bhavana", password: "password")
