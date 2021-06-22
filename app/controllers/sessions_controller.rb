8de2a8efdc9ae68288e9aeb2ec85ae8e1aec503c
Jayanth <jayanth.koralla@gmail.com>
2021-06-22 10:34:36 +0530

    done upto sessions and flash msgs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,44 +2,23 @@ class SessionsController < ApplicationController
   
   def new
   end
-<<<<<<< HEAD
   
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id]=user.id
-      flash[:success] = "You have successfulyy logged in"
-=======
-
-  def create
-    session[:user_id] = nil
-    user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
->>>>>>> feature
       redirect_to root_path
     else
       flash.now[:error] = "There was something wrong with your login information"
       render 'new'
     end
   end
-<<<<<<< HEAD
-  
-  def destroy
-    byebug
-    session[:user_id]= nil
-    flash[:success] = "You have successfully logged out"
-    redirect_to login_path
-  end
-
-=======
 
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
   end
->>>>>>> feature
 end
